31e3394854afbeac68f2c3518bb88640a6440225
Diggerati <innocentmasuki99@gmail.com>
2020-09-15 20:16:18 +0300

    Removed the dependencies from gradle for fresh implementation using volley Library

--- app/src/main/java/com/example/library/signUp.java
+++ app/src/main/java/com/example/library/signUp.java
@@ -15,8 +15,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.vishnusivadas.advanced_httpurlconnection.FetchData;
-import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
 
 public class signUp extends AppCompatActivity {
  Button logInbtn , signUpBtn;
@@ -57,65 +56,8 @@ public class signUp extends AppCompatActivity {
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String username;
-                final String email;
-                final String regNo;
-                final String confirmed;
-                final String newpwd;
-                String pwd = null;
-                username = fullName.getText().toString();
-                regNo = regNumber.getText().toString();
-                email = userMail.getText().toString();
-                newpwd = newPassword.getText().toString();
-                confirmed = newPassword.getText().toString();
-
-                if(newpwd.equals(confirmed)) pwd = confirmed;
-
-
-                if(!username.equals("") && !regNo.equals("") && !email.equals("") && !newpwd.equals("") && !confirmed.equals("")) {
-                    //Start ProgressBar first (Set visibility VISIBLE)
-                    progressbar.setVisibility(View.VISIBLE);
-                    Handler handler = new Handler(Looper.getMainLooper());
-                    final String finalPwd = pwd;
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            //Starting Write and Read data with URL
-                            //Creating array for parameters
-                            String[] field = new String[4];
-                            field[0] = "fullname";
-                            field[1] = "regnumber";
-                            field[2] = "email";
-                            field[3] = "password";
-                            //Creating array for data
-                            String[] data = new String[4];
-                            data[0] = username;
-                            data[1] = regNo;
-                            data[2] = email;
-                            data[3] = finalPwd;
-                            PutData putData = new PutData("http://192.168.137.1/loginRegister/signup.php?_ijt=g017u4ohengs4672n57f2872nq", "POST", field, data);
-                            if (putData.startPut()) {
-                                if (putData.onComplete()) {
-                                    progressbar.setVisibility(View.GONE);
-                                    String result = putData.getResult();
-                                    if(result.equals("Sign Up Success")){
-                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
-                                        startActivity(new Intent(signUp.this, LogIn.class));
-                                        finish();
-
-                                    }
-                                    else{
-                                        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
-                                    }
-                                }
-                            }
-                            //End Write and Read data with URL
-                        }
-                    });
-                }
-                else {
-                    Toast.makeText(getApplicationContext(), "All Fields Must be filled", Toast.LENGTH_SHORT).show();
-                }
+                startActivity(new Intent(signUp.this, AppHome.class));
+                finish();
             }
         });
     }
@@ -123,9 +65,7 @@ public class signUp extends AppCompatActivity {
     public void onBackPressed() {
         // Create the object of
         // AlertDialog Builder class
-        AlertDialog.Builder builder
-                = new AlertDialog
-                .Builder(signUp.this);
+        AlertDialog.Builder builder = new AlertDialog.Builder(signUp.this);
 
         // Set the message show for the Alert time
         builder.setMessage("Do you want to exit ? 😢");
@@ -142,13 +82,8 @@ public class signUp extends AppCompatActivity {
         // OnClickListener method is use of
         // DialogInterface interface.
 
-        builder
-                .setPositiveButton(
-                        "Yes",
-                        new DialogInterface
-                                .OnClickListener() {
-
-                            @Override
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
                             public void onClick(DialogInterface dialog,
                                                 int which)
                             {
